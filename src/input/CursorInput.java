3d39032f1653f41a4d0cb66e27d4f860c0bca6ad
simbaking <changfourafrica@gmail.com>
2016-08-28 13:18:53 -0400

    setting up basic graghics and input

---
+++ src/input/CursorInput.java
@@ -0,0 +1,14 @@
+package input;
+
+import org.lwjgl.glfw.GLFWCursorPosCallback;
+
+public class CursorInput extends GLFWCursorPosCallback {
+
+	@Override
+	public void invoke(long window, double xpos, double ypos) {
+		
+		
+		
+	}
+
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,89 @@
 package main;
 
-public class Main {
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
 
+import org.lwjgl.glfw.GLFWCursorPosCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+
+import input.CursorInput;
+
+
+
+public class Main implements Runnable{
+	
+	private boolean running = false;
+	private Thread thread;
+	
+	private long window;
+	
+	private int winSize;
+	
+	GLFWCursorPosCallback cursorPos;
+	
+	public static void main(String[] args) {
+		new Main().start();
+	}
+	
+	private void start() {
+		
+		running = true;
+		thread = new Thread(this, "Game");
+		thread.start();
+		
+	}
+
+	public void run() {
+		
+		init();
+		
+		while(running){
+			
+			update();
+			render();
+			
+			if(glfwWindowShouldClose(window)){
+				running = false;
+			}
+			
+		}
+		
+	}
+
+	private void init() {
+		
+		if(!glfwInit()){
+			
+			// TODO: handle GL not initializing error
+			
+			return;
+		}
+		
+		glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+		GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+		winSize = (vidmode.height()/4)*3;
+		window = glfwCreateWindow(winSize, winSize, "Tic-tac-toe", NULL, NULL);
+		
+		if(window == NULL){
+			// TODO: handle non initialized window error
+			return;
+		}
+		
+		glfwSetWindowPos(window, (vidmode.width() - winSize)/2, (vidmode.height() - winSize)/2 );
+		glfwMakeContextCurrent(window);
+		glfwShowWindow(window);
+		
+		glfwSetCursorPosCallback(window, cursorPos = new CursorInput());
+		
+	}
+	
+	private void update() {
+		glfwPollEvents();
+	}
+	
+	private void render() {
+		glfwSwapBuffers(window);
+	}
+	
 }
